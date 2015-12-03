f847f34662ed20e36acfdea8bc83f6bb3fd4f4bc
asthanarht <asthanarht@gmail.com>
2015-12-03 00:32:31 -0800

    facts

---
+++ Facts/Facts.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Facts.WinPhone.Resources;
+
+namespace Facts.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Facts/Facts/views/factlistview.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Facts.views
+{
+    public partial class factlistview : ContentPage
+    {
+        public factlistview()
+        {
+            InitializeComponent();
+        }
+    }
+}
